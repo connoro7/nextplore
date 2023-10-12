5d17a09d73feba039e8dc55d13f06fa73c971333
Connor Dillon <connor@connordillon.dev>
2023-10-11 18:56:07 -0700

    Refactor - pull options out of NextAuth handler for cleaner export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,15 +3,17 @@ import GithubProvider from "next-auth/providers/github"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "../../mongoClientPromise"
 
-
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GithubProvider({
         clientId: process.env.GITHUB_CLIENT_ID as string,
         clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     })
   ],
-  adapter: MongoDBAdapter(clientPromise)
-})
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET as string,
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
