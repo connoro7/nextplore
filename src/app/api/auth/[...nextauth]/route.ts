5a5a4fca35f93e996e1d282024a6bf9312376ff0
Connor Dillon <connor@connordillon.dev>
2023-10-11 20:56:48 -0700

    Add link to documentation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import clientPromise from "../../mongoClientPromise"
 
 export const authOptions = {
   providers: [
+    // Documentation: https://next-auth.js.org/providers/github
     GithubProvider({
         clientId: process.env.GITHUB_CLIENT_ID as string,
         clientSecret: process.env.GITHUB_CLIENT_SECRET as string
